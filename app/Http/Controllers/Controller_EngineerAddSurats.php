49776c73794034b9ca2bcb606d78aa61b7e8a259
aufar <aufarr@gmail.com>
2024-01-10 17:04:42 +0700

    ganti algoritma generate random number

--- app/Http/Controllers/Controller_EngineerAddSurats.php
+++ app/Http/Controllers/Controller_EngineerAddSurats.php
@@ -41,36 +41,58 @@ class Controller_EngineerAddSurats extends Controller
         $added_by = Auth::user()->inisial_user;
 
         //No Urutan
-        $suratnew = new Surat();
-
-        $tanggal = date('d');
-        $bulan = date('m');
-        $tahun = date('Y');
-        $lastUrutan = $suratnew->orderBy('id', 'desc')->first();
-        $lastTahun = $lastUrutan ? substr($lastUrutan->no_surat, 5, 2) : null;
-        $newUrutanNumber = ($lastTahun == substr($tahun, -0)) ? intval(substr($lastUrutan->no_surat, -3)) + 1 : 1;
-        $newUrutanFormatted = str_pad($newUrutanNumber, 3, '0', STR_PAD_LEFT);
-        //Sek.ASPI/SR/-Tahun -TanggalBulanUrutanformkeseluruhan
-        $nomorUrutan = 'Sek.ASPI/SR/' . $newUrutanFormatted . '/'. $bulan .'/'. substr($tahun, -0);
-
-        //No Unik
-        $randno1 = random_int(10, 99);
-        $randno2 = random_int(100, 999);
-        $randno3 = random_int(0, 9);
-        $randno4 = random_int(10, 99);
-        $newrandom = $randno1. '.'.$randno2. '.'.$randno3. '.'.$randno4;
         
 
-        $newprojectsurat = Surat::create([ 
-            'id_mitra' => $request->id_mitra,   
-            'no_unik'  => $newrandom,                   
-            'no_surat' => $nomorUrutan,
-            'perihal' => $request->perihal,
-            'waktu_assign_surat' => $request->waktu_assign_surat,
-            'added_by' => $added_by,
-        ]);
 
-        return redirect('/engineer/surat')->with('success','No Surat berhasil di tambahkan');
+        //No Unik
+        $loop = rand(5,7);
+        $randomNumber = "";
+
+        for($i = 0; $i<$loop; $i++){
+            $randomUnit = rand(0,999);
+            $randomNumber .= $randomUnit;
+
+            if($i != $loop-1){
+                $randomNumber .= ".";
+            }
+            else{
+                break;
+            }
+        }
+
+        dd($randomNumber);
+
+        // //No Urutan
+        // $suratnew = new Surat();
+
+        // $tanggal = date('d');
+        // $bulan = date('m');
+        // $tahun = date('Y');
+        // $lastUrutan = $suratnew->orderBy('id', 'desc')->first();
+        // $lastTahun = $lastUrutan ? substr($lastUrutan->no_surat, 5, 2) : null;
+        // $newUrutanNumber = ($lastTahun == substr($tahun, -0)) ? intval(substr($lastUrutan->no_surat, -3)) + 1 : 1;
+        // $newUrutanFormatted = str_pad($newUrutanNumber, 3, '0', STR_PAD_LEFT);
+        // //Sek.ASPI/SR/-Tahun -TanggalBulanUrutanformkeseluruhan
+        // $nomorUrutan = 'Sek.ASPI/SR/' . $newUrutanFormatted . '/'. $bulan .'/'. substr($tahun, -0);
+
+        // //No Unik
+        // $randno1 = random_int(10, 99);
+        // $randno2 = random_int(100, 999);
+        // $randno3 = random_int(0, 9);
+        // $randno4 = random_int(10, 99);
+        // $newrandom = $randno1. '.'.$randno2. '.'.$randno3. '.'.$randno4;
+        
+
+        // $newprojectsurat = Surat::create([ 
+        //     'id_mitra' => $request->id_mitra,   
+        //     'no_unik'  => $newrandom,                   
+        //     'no_surat' => $nomorUrutan,
+        //     'perihal' => $request->perihal,
+        //     'waktu_assign_surat' => $request->waktu_assign_surat,
+        //     'added_by' => $added_by,
+        // ]);
+
+        // return redirect('/engineer/surat')->with('success','No Surat berhasil di tambahkan');
     }
 
     public function getUser(){                                      //ngambil data user engineer dan adminxengineer
