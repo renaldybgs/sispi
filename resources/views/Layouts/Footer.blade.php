180a4f86413ac06af2c0f19f5d174ba25ccd1793
renaldybgs <renaaldybagas13@gmail.com>
2024-01-17 09:56:16 +0700

    logo

--- resources/views/Layouts/Footer.blade.php
+++ resources/views/Layouts/Footer.blade.php
@@ -1,8 +1,8 @@
 
 <footer class="main-footer">
-    <strong>Copyright &copy; 2023 <a>ASPI</a>.</strong>
+    <strong>Copyright &copy; 2024 <a>ASPI</a>.</strong>
     All rights reserved. 
     <div class="float-right d-none d-sm-inline-block">
-        <b>Asosiasi Sistem Pembayaran Indonesia </b> <a>| Version</a> <b>1.3</b>
+        <b>Asosiasi Sistem Pembayaran Indonesia </b> <a>| Version</a> <b>1.4</b>
     </div>
 </footer>
